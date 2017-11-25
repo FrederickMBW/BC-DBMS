d80e204a14d02ddb11e6c4dd63322fa63c23ff6b
FrederickMBW <32556106+FrederickMBW@users.noreply.github.com>
2017-11-25 14:31:31 -0800

    Added Parameters
    Added Parameter to make more custom for every popup.

--- popUp.java
+++ popUp.java
@@ -5,17 +5,22 @@
 
 //package SM;
 
+import java.awt.Dimension;
+
 import javax.swing.*;
 
 public class PopUp extends JFrame{
 
-    public PopUp(JScrollPane sPane, String TITLE){
+    public PopUp(JScrollPane sPane, String TITLE, int width, int height){
         this.setTitle(TITLE);
         
-        setSize(800,400);
-        setLocation(600,300);
+        setSize(width + 100, height + 50);
+        setLocation(0, 0);
 
+        Dimension dim = new Dimension(width, height);
+        
         JPanel p = new JPanel();
+        sPane.setPreferredSize(dim);
         p.add(sPane);
         add(p);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
